e0fdf6066930ec735c687cede6624fa7a0451ead
yashkumar84 <166642691+yashkumar84@users.noreply.github.com>
2025-09-12 17:04:14 +0530

    Add files via upload

---
+++ match-case.py
@@ -0,0 +1,11 @@
+a = "yash"
+
+match a:
+    case "yash":
+        print('Hello Dear you have pressed 1')
+    
+    case 4:
+        print('Hello Dear you have pressed 2')
+
+    case 5:
+        print('Hello Dear you have pressed 2')
